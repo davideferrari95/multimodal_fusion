4d4da6745fbb2885b4029817b7d76bc241828cd4
davideferrari95 <davideferr@unimore.it>
2023-06-14 19:36:47 +0200

    Robot Functions moved to separate file `move_robot.py`

--- scripts/Fusion_node_withPS.py
+++ scripts/Fusion_node_withPS.py
@@ -4,22 +4,13 @@ import rospy, rospkg
 import os, threading, time
 import torch
 
-from typing import List
 from collections import Counter
 
 # Import ROS Messages
 from geometry_msgs.msg import Pose
-from sensor_msgs.msg import JointState
-from std_msgs.msg import Int32MultiArray, String, Bool 
-from trajectory_msgs.msg import JointTrajectoryPoint
-from ur_rtde_controller.msg import CartesianPoint
+from std_msgs.msg import Int32MultiArray, String
 
-# Import ROS Services
-from std_srvs.srv import Trigger, TriggerRequest 
-from ur_rtde_controller.srv import RobotiQGripperControl, RobotiQGripperControlRequest
-from ur_rtde_controller.srv import GetForwardKinematic, GetForwardKinematicRequest, GetForwardKinematicResponse
-from ur_rtde_controller.srv import GetInverseKinematic, GetInverseKinematicRequest, GetInverseKinematicResponse
-
+from move_robot import UR10e_RTDE_Move
 from neural_classifier import LitNeuralNet
 from utils import OBJECTS, RIGHT_AREA, LEFT_AREA
 from utils import defaultPos, placePos, intermediatePos
@@ -34,30 +25,18 @@ class Fusion:
         # Get Package Path
         package_path = rospkg.RosPack().get_path('fusion')
 
+        # Instance Robot Movement Class
+        self.robot = UR10e_RTDE_Move()
+
         # Publishers
-        # self.ur10Pub=rospy.Publisher('/ur_rtde/controllers/joint_space_controller/command',JointTrajectoryPoint,queue_size=10)
-        # self.ur10PubCartesian=rospy.Publisher('/ur_rtde/controllers/cartesian_space_controller/command',CartesianPoint,queue_size=10)
-        self.ur10Pub=rospy.Publisher('/desired_joint_pose', JointState, queue_size=1)
-        # self.ur10PubCartesian=rospy.Publisher('/desired_tcp_pose', Pose, queue_size=1)
         self.ttsPub = rospy.Publisher('/tts',String,queue_size=1)
-
-        # Init Gripper Service
-        self.gripper_srv = rospy.ServiceProxy('/ur_rtde/robotiq_gripper/command', RobotiQGripperControl)
-
-        # IK, FK Services
-        self.get_FK_srv = rospy.ServiceProxy('ur_rtde/getFK', GetForwardKinematic)
-        self.get_IK_srv = rospy.ServiceProxy('ur_rtde/getIK', GetInverseKinematic)
-
-        # Stop Robot Service
-        self.stop_service = rospy.ServiceProxy('/ur_rtde/controllers/stop_robot', Trigger)
-        self.stop_req = TriggerRequest()
 
         # Subscribers
         rospy.Subscriber("voice",   Int32MultiArray, self.voiceCallback)
         rospy.Subscriber("gesture", Int32MultiArray, self.gestureCallback)
         rospy.Subscriber("area",    Int32MultiArray, self.areaCallback)
 
-        # init network
+        # Init Classifier Neural Network
         self.model = LitNeuralNet(0.8,0.1,0.1,2,32,64,35)
         state_dict = torch.load(os.path.join(package_path, 'model/fusion_model.pth'))
         self.model.load_state_dict(state_dict)
@@ -137,86 +116,16 @@ class Fusion:
         # Set Area Flag
         self.areaCheck = True
 
-    def move_joint(self, joint_positions:List[float]):
-
-        """ Joint Space Movement """
-
-        # Destination Position (if `time_from_start` = 0 -> read velocity[0])
-        pos = JointState()
-        pos.position = joint_positions
-
-        # pos = JointTrajectoryPoint()
-        # pos.time_from_start = rospy.Duration(0)
-        # pos.velocities = [0.4]
-
-        # Publish Joint Position
-        self.ur10Pub.publish(pos)
-
-        # flag = rospy.wait_for_message('/ur_rtde/trajectory_executed', Bool)
-        flag = rospy.wait_for_message('/trajectory_execution', Bool)
-
-        # Exception with Trajectory Execution
-        if flag.data is not True: raise Exception("ERROR: An exception occurred during Trajectory Execution")
-
-    def move_cartesian(self, tcp_position:Pose):
-
-        """ Cartesian Movement -> Converted in Joint Movement with IK """
-
-        # Call Inverse Kinematic
-        joint_position = self.IK(tcp_position)
-
-        # Joint Space Movement
-        self.move_joint(joint_position)
-
-    def FK(self, joint_positions:List[float]) -> Pose:
-
-        # Set Forward Kinematic Request
-        req = GetForwardKinematicRequest()
-        req.joint_position = joint_positions
-
-        # Call Forward Kinematic
-        rospy.wait_for_service('ur_rtde/getFK')
-        res: GetForwardKinematicResponse = self.get_FK_srv(req)
-
-        return res.tcp_position
-
-    def IK(self, pose:Pose) -> List[float]:
-
-        # Set Inverse Kinematic Request
-        req = GetInverseKinematicRequest()
-        req.tcp_position = pose
-
-        # Call Inverse Kinematic
-        rospy.wait_for_service('ur_rtde/getIK')
-        res: GetInverseKinematicResponse = self.get_IK_srv(req)
-
-        return res.joint_position
-
-    def move_gripper(self, position):
-
-        """ Open-Close Gripper Function """
-
-        # Set Gripper Request
-        req = RobotiQGripperControlRequest()
-        req.position, req.speed, req.force = position, 100, 25
-
-        # Call Gripper Service
-        rospy.wait_for_service('/ur_rtde/robotiq_gripper/command')
-        res = self.gripper_srv(req)
-
-        '''if gripper_response.status != "2":
-        raise Exception("Sorry, An exception occurred")'''
-
     def handover(self, tts_start, position, start_grip, end_grip, tts_end):
 
         # Node-RED Start TTS
         self.ttsPub.publish(tts_start)
 
         # Move Gripper to Starting Position (Open/Close for Internal/External Gripping)
-        self.move_gripper(start_grip)
+        self.robot.move_gripper(start_grip)
 
         # Move to Intermediate Position
-        self.move_joint(intermediatePos)
+        self.robot.move_joint(intermediatePos)
 
         # Forward Kinematic -> Increase z + 40cm
         cartesian_pose: Pose() = self.FK(position)
@@ -229,55 +138,49 @@ class Fusion:
         # cartesian_pose.velocity = 0.2
 
         # Cartesian Movement -> 40cm Over the Object
-        self.move_cartesian(cartesian_pose)
+        self.robot.move_cartesian(cartesian_pose)
 
         # Move to Object
-        self.move_joint(position)
+        self.robot.move_joint(position)
         time.sleep(1)
 
         # Grip Object
-        self.move_gripper(end_grip)
+        self.robot.move_gripper(end_grip)
 
         # Cartesian Movement -> 40cm Over the Object
-        self.move_cartesian(cartesian_pose)
+        self.robot.move_cartesian(cartesian_pose)
         time.sleep(0.5)
 
         # Move to Intermediate Position
-        self.move_joint(intermediatePos)
+        self.robot.move_joint(intermediatePos)
 
         # Forward Kinematic -> Increase z + 40cm
         cartesian_pose: Pose() = self.FK(placePos)
         cartesian_pose.position.z += 0.40
         
         # Cartesian Movement -> 40cm Over the Object
-        self.move_cartesian(cartesian_pose)
+        self.robot.move_cartesian(cartesian_pose)
         time.sleep(1)
 
         # Move to Place Position
-        self.move_joint(placePos)
+        self.robot.move_joint(placePos)
 
         # Release Object
-        self.move_gripper(start_grip)
+        self.robot.move_gripper(start_grip)
 
         # Forward Kinematic -> Increase z + 20cm
         cartesian_pose: Pose() = self.FK(placePos)
         cartesian_pose.position.z += 0.20
 
         # Cartesian Movement -> 20cm Over the Object
-        self.move_cartesian(cartesian_pose)
+        self.robot.move_cartesian(cartesian_pose)
 
         # Node-RED TTS Response
         self.ttsPub.publish(tts_end)
         time.sleep(1)
 
         # Move to Home
-        self.move_joint(defaultPos)
-
-    def stopRobot(self):
-
-        rospy.wait_for_service('/ur_rtde/controllers/stop_robot')
-        self.stop_req = TriggerRequest()
-        self.stop_response = self.stop_service(self.stop_req)
+        self.robot.move_joint(defaultPos)
 
     def cleaner(self):
          
---
+++ scripts/move_robot.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import rospy
+from typing import List
+
+# Import ROS Messages
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectoryPoint
+from ur_rtde_controller.msg import CartesianPoint
+
+# Import ROS Services
+from std_srvs.srv import Trigger, TriggerRequest 
+from ur_rtde_controller.srv import RobotiQGripperControl, RobotiQGripperControlRequest
+from ur_rtde_controller.srv import GetForwardKinematic, GetForwardKinematicRequest, GetForwardKinematicResponse
+from ur_rtde_controller.srv import GetInverseKinematic, GetInverseKinematicRequest, GetInverseKinematicResponse
+
+class UR10e_RTDE_Move():
+    
+    def __init__(self):
+
+        # Publishers
+        # self.ur10Pub=rospy.Publisher('/ur_rtde/controllers/joint_space_controller/command',JointTrajectoryPoint,queue_size=10)
+        # self.ur10PubCartesian=rospy.Publisher('/ur_rtde/controllers/cartesian_space_controller/command',CartesianPoint,queue_size=10)
+        self.ur10Pub=rospy.Publisher('/desired_joint_pose', JointState, queue_size=1)
+        # self.ur10PubCartesian=rospy.Publisher('/desired_tcp_pose', Pose, queue_size=1)
+
+        # Init Gripper Service
+        self.gripper_srv = rospy.ServiceProxy('/ur_rtde/robotiq_gripper/command', RobotiQGripperControl)
+
+        # IK, FK Services
+        self.get_FK_srv = rospy.ServiceProxy('ur_rtde/getFK', GetForwardKinematic)
+        self.get_IK_srv = rospy.ServiceProxy('ur_rtde/getIK', GetInverseKinematic)
+
+        # Stop Robot Service
+        self.stop_service = rospy.ServiceProxy('/ur_rtde/controllers/stop_robot', Trigger)
+        self.stop_req = TriggerRequest()
+
+    def move_joint(self, joint_positions:List[float]):
+
+        """ Joint Space Movement """
+
+        # Destination Position (if `time_from_start` = 0 -> read velocity[0])
+        pos = JointState()
+        pos.position = joint_positions
+
+        # pos = JointTrajectoryPoint()
+        # pos.time_from_start = rospy.Duration(0)
+        # pos.velocities = [0.4]
+
+        # Publish Joint Position
+        self.ur10Pub.publish(pos)
+
+        # flag = rospy.wait_for_message('/ur_rtde/trajectory_executed', Bool)
+        flag = rospy.wait_for_message('/trajectory_execution', Bool)
+
+        # Exception with Trajectory Execution
+        if flag.data is not True: raise Exception("ERROR: An exception occurred during Trajectory Execution")
+
+    def move_cartesian(self, tcp_position:Pose):
+
+        """ Cartesian Movement -> Converted in Joint Movement with IK """
+
+        # Call Inverse Kinematic
+        joint_position = self.IK(tcp_position)
+
+        # Joint Space Movement
+        self.move_joint(joint_position)
+
+    def FK(self, joint_positions:List[float]) -> Pose:
+
+        # Set Forward Kinematic Request
+        req = GetForwardKinematicRequest()
+        req.joint_position = joint_positions
+
+        # Call Forward Kinematic
+        rospy.wait_for_service('ur_rtde/getFK')
+        res: GetForwardKinematicResponse = self.get_FK_srv(req)
+
+        return res.tcp_position
+
+    def IK(self, pose:Pose) -> List[float]:
+
+        # Set Inverse Kinematic Request
+        req = GetInverseKinematicRequest()
+        req.tcp_position = pose
+
+        # Call Inverse Kinematic
+        rospy.wait_for_service('ur_rtde/getIK')
+        res: GetInverseKinematicResponse = self.get_IK_srv(req)
+
+        return res.joint_position
+
+    def move_gripper(self, position):
+
+        """ Open-Close Gripper Function """
+
+        # Set Gripper Request
+        req = RobotiQGripperControlRequest()
+        req.position, req.speed, req.force = position, 100, 25
+
+        # Call Gripper Service
+        rospy.wait_for_service('/ur_rtde/robotiq_gripper/command')
+        res = self.gripper_srv(req)
+
+        '''if gripper_response.status != "2":
+        raise Exception("Sorry, An exception occurred")'''
+        
+    def stopRobot(self):
+
+        rospy.wait_for_service('/ur_rtde/controllers/stop_robot')
+        self.stop_req = TriggerRequest()
+        self.stop_response = self.stop_service(self.stop_req)
